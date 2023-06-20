6f06f599f3d8bead0845356cf23158bebeab7ef4
vuongtunglam1406 <vuongtunglam1406@gmail.com>
2023-06-20 13:42:59 +0700

    boss shooting

--- PRU211m_GroupProject_Flyer/Assets/Scripts/PlayerScript.cs
+++ PRU211m_GroupProject_Flyer/Assets/Scripts/PlayerScript.cs
@@ -42,11 +42,11 @@ public class PlayerScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Enemy"))
+        /*if(collision.gameObject.CompareTag("Enemy"))
         {
             Destroy(gameObject);
             Debug.Log("Hit pipe");
-        }
+        }*/
     }
 
     private void Flicker(GameObject gameObject)
