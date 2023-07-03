3406629176e6d8834b7bd25641477d5f5be05e1d
haind <haindhe141482@fpt.edu.vn>
2023-07-03 15:26:29 +0700

    Pause + Resume Button
    Create Pause + Resume Button

---
+++ PRU211m_GroupProject_Flyer/Assets/Scripts/MenuFunction.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuFunction : MonoBehaviour
+{
+    [SerializeField] bool isPaused = false;
+    private PipeMove[] pipes;
+    [SerializeField] GameObject pauseMenu;
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitMain()
+    {
+        Application.Quit();
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f; // Tạm dừng thời gian trong game
+            // Hiển thị giao diện hoặc thực hiện các hành động liên quan khi game tạm dừng
+
+            // Tạm dừng hoạt động của tất cả các đối tượng PipeMove
+            pipes = FindObjectsOfType<PipeMove>();
+            foreach (var pipe in pipes)
+            {
+                pipe.Pause();
+                pauseMenu.SetActive(true);
+            }
+        }
+        else
+        {
+            Time.timeScale = 1f; // Tiếp tục thời gian trong game
+            // Ẩn giao diện hoặc thực hiện các hành động liên quan khi game tiếp tục
+
+            // Tiếp tục hoạt động của tất cả các đối tượng PipeMove
+            if (pipes != null)
+            {
+                foreach (var pipe in pipes)
+                {
+                    pipe.Resume();
+                    pauseMenu.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);   
+    }
+}
