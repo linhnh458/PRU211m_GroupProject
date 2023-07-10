7e81cc7a50d681ca176dcec89979cac040107d4c
vuongtunglam1406 <vuongtunglam1406@gmail.com>
2023-07-10 09:07:57 +0700

    fixed

--- PRU211m_GroupProject_Flyer/Assets/Scripts/Pipe/PipeMove.cs
+++ PRU211m_GroupProject_Flyer/Assets/Scripts/Pipe/PipeMove.cs
@@ -4,21 +4,55 @@ using UnityEngine;
 
 public class PipeMove : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 0.01f;
+    public float moveSpeed = 0.05f;
     float deadZone = -20;
+    private bool isPaused = false;
+    private Vector3 velocity;
+    float speedUpRate = 30f;
+    float timer = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        Move();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (!isPaused)
+        {
+            Move();
+        }
+    }
+
+    void Move()
     {
         transform.position += Vector3.left * moveSpeed;
         if (transform.position.x < deadZone)
         {
             Destroy(gameObject);
         }
+        if (timer < speedUpRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            moveSpeed = moveSpeed + moveSpeed * 0.2f;
+            timer = 0;
+        }
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        velocity = Vector3.zero;
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+        velocity = Vector3.left * moveSpeed;
     }
 }
