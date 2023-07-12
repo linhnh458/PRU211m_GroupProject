36c582162aa36517aa62087c6696f97eeecb0b07
vuongtunglam1406 <vuongtunglam1406@gmail.com>
2023-07-12 14:52:59 +0700

    upload frog png and background

---
+++ PRU211m_GroupProject_Flyer/Assets/Scripts/BackGround/BackGroundScroller.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    private Renderer renderer;
+    private Vector2 saveOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        Vector2 offset = new Vector2(x, 0);
+        renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
