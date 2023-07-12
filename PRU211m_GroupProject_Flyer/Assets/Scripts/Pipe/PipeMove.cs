6b0ac7384d9119fe6bb18348492a200944dcd958
vuongtunglam1406 <vuongtunglam1406@gmail.com>
2023-07-12 13:34:31 +0700

    fixed speed up map

--- PRU211m_GroupProject_Flyer/Assets/Scripts/Pipe/PipeMove.cs
+++ PRU211m_GroupProject_Flyer/Assets/Scripts/Pipe/PipeMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PipeMove : MonoBehaviour
 {
-    public float moveSpeed = 0.05f;
+    public float moveSpeed = 0.01f;
     float deadZone = -20;
     private bool isPaused = false;
     private Vector3 velocity;
